e8bab40c8605dffbc105cbf0e59596c23d61370f
Andy Teh <ateh0701@gmail.com>
2024-01-15 14:16:10 -0800

    added mse and weighted average of mse

--- tree.py
+++ tree.py
@@ -1,15 +1,30 @@
 import pandas as pd
 
-boston = pd.read_csv('boston.csv').drop('Unnamed: 0', axis=1)
+included_columns = ['CRIM', 'RM', 'Price']
+boston = pd.read_csv('boston.csv').loc[:, included_columns]
+
+
+def mean_squared_error(target_column):
+    average = target_column.mean()
+    return ((target_column-average) ** 2).mean()
+
+
+def weighted_average_of_mse(target_columns):
+    result = 0
+    for col in target_columns:
+        result += mean_squared_error(col)/len(target_columns)
+    return result / sum(map(len, target_columns))
 
 
 def tree(dataset, target, frac=1.):
     dataset = dataset.sample(frac=frac, replace=False)
+    N = len(dataset)
     features = [col for col in dataset.columns if col != target]
-    for col in features:
-        dataset.sort_values(col, inplace=True)
-        for row in dataset:
-            print(234234, row)
+    for feature in features:
+        dataset.sort_values(feature, inplace=True)
+        for row_index in range(1, N):
+            print(row_index, feature, weighted_average_of_mse(
+                (dataset[target][:row_index], dataset[target][row_index:])))
     return dataset
 
 

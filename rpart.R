c5119bc16735648cc195bc0587997651b1938250
Andy Teh <ateh0701@gmail.com>
2024-04-04 11:27:32 -0700

    add bootstrapping to rpart

--- rpart.R
+++ rpart.R
@@ -1,15 +1,19 @@
 # LOAD NECESSARY LIBRARIES
-# library(ISLR) # contains Hitters dataset
 library(MASS)
 library(rpart) # for fitting decision trees
 library(rpart.plot) # for plotting decision trees
+library(boot)
 
-set.seed(1)
+set.seed(123)
+
+# Create a training and test set
+test_indices <- sample(seq_len(nrow(Boston)), nrow(Boston) * 0.2)
+test_data <- Boston[test_indices, ]
+train_data <- Boston[-test_indices, ]
 
 # REGRESSION TREE
 # build the initial tree
-# tree <- rpart(Salary ~ Years + HmRun, data = Hitters, control = rpart.control(cp = .0001))
-tree <- rpart(medv ~ crim + zn + indus, data = Boston, control = rpart.control(cp = .0001))
+tree <- rpart(medv ~ ., data = train_data, control = rpart.control(cp = 0.0001))
 
 # view results of model
 # printcp(tree)
@@ -24,16 +28,38 @@ pruned_tree <- prune(tree, cp = best)
 prp(pruned_tree,
   faclen = 0, # use full names for factor labels
   extra = 1, # display number of obs. for each terminal node
-  roundint = F, # don't round to integers in output
+  roundint = FALSE, # don't round to integers in output
   digits = 5
 ) # display 5 decimal places in output
 
-# new <- data.frame(Years = 7, HmRun = 4)
-new <- data.frame(
-  crim = c(0.04741, 0.06617),
-  zn = c(0, 0),
-  indus = c(11.9, 3.24),
-  medv = c(11.9, 19.3)
+# Generate predictions on the test subset
+test_predictions <- predict(pruned_tree, newdata = test_data)
+
+# Print the predictions
+# print(test_predictions)
+
+predictions <- function(formula, data, indices) {
+  bootstrap_sample <- data[indices, ]
+  tree <- rpart(
+    formula,
+    bootstrap_sample,
+    control = rpart.control(cp = 0.0001)
+  )
+  best <- tree$cptable[which.min(tree$cptable[, "xerror"]), "CP"]
+  pruned_tree <- prune(tree, cp = best)
+  test_predictions <- predict(pruned_tree, newdata = data)
+  test_predictions
+}
+
+reps <- boot(
+  Boston,
+  predictions,
+  5,
+  formula = medv ~ .
 )
-predictions <- predict(pruned_tree, newdata = new)
-print(predictions)
+
+# print(reps)
+plot(reps)
+
+Boston_boot <- cbind(Boston, t(reps$t))
+# print(Boston_boot)

ca282baee3571e3ea655c8e024b8d7fa0eaf25b8
Andy Teh <ateh0701@gmail.com>
2024-08-28 18:24:53 -0500

    a

--- models_sklearn.py
+++ models_sklearn.py
@@ -10,6 +10,53 @@ class MLP(MLPRegressor):
     pass
 
 
+class Ensemble(RandomForestRegressor):
+    pass
+
+
+class Tree(Ensemble):
+    def __init__(
+        self,
+        *,
+        criterion: Literal['squared_error', 'absolute_error', 'friedman_mse',
+                           'poisson'] = 'squared_error',
+        max_depth=None,
+        min_samples_split=2,
+        min_samples_leaf=1,
+        min_weight_fraction_leaf=0.0,
+        max_features=1.0,
+        max_leaf_nodes=None,
+        min_impurity_decrease=0.0,
+        bootstrap=True,
+        oob_score=False,
+        n_jobs=None,
+        random_state=None,
+        verbose=0,
+        warm_start=False,
+        ccp_alpha=0.0,
+        max_samples=None,
+    ):
+        super().__init__(
+            n_estimators=1,
+            criterion=criterion,
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+            min_weight_fraction_leaf=min_weight_fraction_leaf,
+            max_features=max_features,
+            max_leaf_nodes=max_leaf_nodes,
+            min_impurity_decrease=min_impurity_decrease,
+            bootstrap=bootstrap,
+            oob_score=oob_score,
+            n_jobs=n_jobs,
+            random_state=random_state,
+            verbose=verbose,
+            warm_start=warm_start,
+            ccp_alpha=ccp_alpha,
+            max_samples=max_samples,
+        )
+
+
 class FONN1(MLP):
     def __init__(self,
                  num_trees=10,
@@ -66,7 +113,7 @@ class FONN1(MLP):
             max_fun=max_fun,
         )
         self.num_trees = num_trees
-        self.ensemble = RandomForestRegressor(num_trees)
+        self.ensemble = Ensemble(num_trees)
 
     def _concat_tree(self, X):
         return np.hstack((X, np.column_stack([e.predict(X) for e in self.ensemble.estimators_])))
@@ -192,7 +239,7 @@ class FONN2(MLP):
             max_fun=max_fun,
         )
         self.num_trees = num_trees
-        self.ensemble = RandomForestRegressor(num_trees)
+        self.ensemble = Ensemble(num_trees)
 
     def _concat_tree(self, activations):
         tree_outputs = np.column_stack(
@@ -361,3 +408,4 @@ if __name__ == '__main__':
     # plot_loss(fonn2, 'FONN2')
     # plot_loss(treenn1, 'TREENN1')
     # plot_loss(treenn2, 'TREENN2')
+# tune, rank

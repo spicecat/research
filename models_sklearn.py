940cb38057bbbd61d8e590e892d9927a7b47b250
Andy Teh <ateh0701@gmail.com>
2024-11-16 20:46:22 -0800

    scaler option

--- models_sklearn.py
+++ models_sklearn.py
@@ -1,14 +1,57 @@
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.neural_network import MLPRegressor
 from typing import Literal
 
 
 class Ensemble(RandomForestRegressor):
-    def predict(self, X, stack=False):
-        if stack:
+    def __init__(
+            self,
+            n_estimators=100,
+            *,
+            stack=False,
+            criterion: Literal['squared_error', 'absolute_error',
+                               'friedman_mse', 'poisson'] = 'squared_error',
+            max_depth=None,
+            min_samples_split=2,
+            min_samples_leaf=1,
+            min_weight_fraction_leaf=0.0,
+            max_features=1.0,
+            max_leaf_nodes=None,
+            min_impurity_decrease=0.0,
+            bootstrap=True,
+            oob_score=False,
+            n_jobs=None,
+            random_state=None,
+            verbose=0,
+            warm_start=False,
+            ccp_alpha=0.0,
+            max_samples=None,
+    ):
+        super().__init__(
+            n_estimators=n_estimators,
+            criterion=criterion,
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+            min_weight_fraction_leaf=min_weight_fraction_leaf,
+            max_features=max_features,
+            max_leaf_nodes=max_leaf_nodes,
+            min_impurity_decrease=min_impurity_decrease,
+            bootstrap=bootstrap,
+            oob_score=oob_score,
+            n_jobs=n_jobs,
+            random_state=random_state,
+            verbose=verbose,
+            warm_start=warm_start,
+            ccp_alpha=ccp_alpha,
+            max_samples=max_samples,
+        )
+        self.stack = stack
+
+    def predict(self, X):
+        if self.stack:
             return np.column_stack([e.predict(X) for e in self.estimators_])
         return super().predict(X)
 
@@ -19,25 +62,25 @@ class MLP_sk(MLPRegressor):
 
 class Tree(Ensemble):
     def __init__(
-        self,
-        *,
-        criterion: Literal['squared_error', 'absolute_error', 'friedman_mse',
-                           'poisson'] = 'squared_error',
-        max_depth=None,
-        min_samples_split=2,
-        min_samples_leaf=1,
-        min_weight_fraction_leaf=0.0,
-        max_features=1.0,
-        max_leaf_nodes=None,
-        min_impurity_decrease=0.0,
-        bootstrap=True,
-        oob_score=False,
-        n_jobs=None,
-        random_state=None,
-        verbose=0,
-        warm_start=False,
-        ccp_alpha=0.0,
-        max_samples=None,
+            self,
+            *,
+            criterion: Literal['squared_error', 'absolute_error', 'friedman_mse',
+                               'poisson'] = 'squared_error',
+            max_depth=None,
+            min_samples_split=2,
+            min_samples_leaf=1,
+            min_weight_fraction_leaf=0.0,
+            max_features=1.0,
+            max_leaf_nodes=None,
+            min_impurity_decrease=0.0,
+            bootstrap=True,
+            oob_score=False,
+            n_jobs=None,
+            random_state=None,
+            verbose=0,
+            warm_start=False,
+            ccp_alpha=0.0,
+            max_samples=None,
     ):
         super().__init__(
             n_estimators=1,
@@ -116,12 +159,15 @@ class FONN1_sk(MLP_sk):
             max_fun=max_fun,
         )
         self.num_trees = num_trees
-        self.ensemble = Ensemble(num_trees)
+        self.ensemble = Ensemble(num_trees, stack=True)
+        # self.scaler_y = StandardScaler()
 
     def _concat_tree(self, X):
-        return np.hstack((self.ensemble.predict(X, stack=True), X))
+        return np.hstack((self.ensemble.predict(X), X))
+        # return np.hstack((scaler_y.transform(self.ensemble.predict(X).reshape(-1, 1).ravel()), X))
 
     def fit(self, X: np.ndarray, y):
+        # self.scaler_y.fit(y.reshape(-1, 1).ravel())
         self.ensemble.fit(X, y)
         return super().fit(self._concat_tree(X), y)
 
@@ -242,12 +288,12 @@ class FONN2_sk(MLP_sk):
             max_fun=max_fun,
         )
         self.num_trees = num_trees
-        self.ensemble = Ensemble(num_trees)
+        self.ensemble = Ensemble(num_trees, stack=True)
 
     def _concat_tree(self, activations):
-        tree_outputs = self.ensemble.predict(activations[0], stack=True)
+        tree_outputs = self.ensemble.predict(activations[0])
         activations[-2] = np.hstack((tree_outputs,
-                                    activations[-2][:, self.num_trees:]))
+                                     activations[-2][:, self.num_trees:]))
         return activations
 
     def _forward_pass(self, activations):
